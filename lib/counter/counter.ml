2219efe848a500d5cd6c42ce33c8d143d6fb0e1b
Isaac-DeFrain <isaacdefrain@gmail.com>
2021-05-05 16:46:46 -0600

    Liveness-checking: add eventually follows property

--- lib/counter/counter.ml
+++ lib/counter/counter.ml
@@ -1,34 +1,45 @@
 (* counter state *)
-type state = {
-  mutable value : int;
-  mutable step : int;
-  mutable continue : bool;
-}
+type state = {mutable value : int; mutable step : int; mutable continue : bool}
 
 (* initial counter state *)
-let counter = {
-  value = 0;
-  step = 0;
-  continue = true;
-}
+let counter = {value = 0; step = 0; continue = true}
 
 (* actions *)
-let incr s = s.value <- s.value + 1 ; s.step <- s.step + 1 ; s
+let incr v = v + 1
 
-let stop s = s.continue <- false ; s
+let decr v = v - 1
 
-(* property *)
-type property = {
+let change f s =
+  s.value <- f s.value ;
+  s.step <- s.step + 1 ;
+  s
+
+let stop s =
+  s.continue <- false ;
+  s
+
+(* eventuality (<>) property *)
+type eventuality_property = {
   mutable satisfy : bool;
   predicate : state -> bool;
   mutable state : int;
 }
 
-let state_predicate s = s.value > 2
-
 (* <>state_predicate *)
-let liveness_property = {
-  satisfy = false;
-  predicate = state_predicate;
-  state = 0;
+let liveness_property pred = {satisfy = false; predicate = pred; state = 0}
+
+(* eventually follows (~>) property *)
+type eventually_follows_property = {
+  mutable conditional_satisfy : bool;
+  conditional : state -> bool;
+  mutable satisfy : bool;
+  predicate : state -> bool;
 }
+
+let follows_property cond pred =
+  {
+    conditional_satisfy = false;
+    conditional = cond;
+    satisfy = false;
+    predicate = pred;
+  }
--- test/liveness.ml
+++ test/liveness.ml
@@ -1,50 +1,66 @@
 open Counter
 
 let action_of_int = function
-  | x when x < 5 ->
-      incr
-  | 5 ->
+  | x when x < 3 ->
+      change incr
+  | x when x >= 3 && x < 6 ->
+      change decr
+  | 6 ->
       stop
   | _ ->
       assert false
 
-let print_res state prop =
+let print_res state (prop : eventuality_property) =
   if prop.satisfy then
     Printf.printf
-      "Liveness property satisfied:\n  final counter value %d\n"
+      "Liveness property satisfied:\n\
+      \  final counter value: %d\n\
+      \  satisfied in state:  %d\n"
       state.value
+      prop.state
   else
     Printf.printf
-      "Liveness property violated:\n  final counter value %d\n"
+      "Liveness property violated:\n  final counter value: %d\n"
       state.value
 
-let reset ctr prop =
+let reset ctr (prop : eventuality_property) =
   ctr.value <- 0 ;
   ctr.step <- 0 ;
   ctr.continue <- true ;
   prop.satisfy <- false ;
   prop.state <- 0
 
-let rec diamond ctr prop =
-  if not ctr.continue then (
-    print_res ctr prop ;
-    reset counter liveness_property )
-  else
-    let update_state = action_of_int @@ Random.int 6 in
-    let updated_prop =
-      if not prop.satisfy then prop.satisfy <- prop.predicate ctr ;
-      prop
-    in
-    diamond (update_state ctr) updated_prop
+(* state predicate *)
+let state_predicate s = s.value > 2
 
-let () = Random.init @@ Random.int 10
+(* TODO somewhow the prop.state always returns 6... *)
+let diamond pred ctr =
+  let liveness_prop = liveness_property pred in
+  let rec loop prop ctr =
+    if not ctr.continue then (print_res ctr prop ; reset counter prop)
+    else
+      let sat = prop.predicate ctr in
+      if (not prop.satisfy) && sat then (
+        prop.satisfy <- sat ;
+        prop.state <- ctr.step ) ;
+      let update_state =
+        let r = Random.int 6 in
+        if ctr.step > 5 then ctr.continue <- false ;
+        if r >= 3 && r < 6 && ctr.value = 0 then fun x -> x
+        else action_of_int r
+      in
+      loop prop (update_state ctr)
+  in
+  loop liveness_prop ctr
 
-let () = diamond counter liveness_property
+let () = Random.init @@ Random.int 100
 
-let () = diamond counter liveness_property
+let () = diamond state_predicate counter
 
-let () = diamond counter liveness_property
+let () = diamond state_predicate counter
 
-let () = diamond counter liveness_property
+let () = diamond state_predicate counter
 
-let () = diamond counter liveness_property
+let () = diamond state_predicate counter
+
+let () = diamond state_predicate counter
